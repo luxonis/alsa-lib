4c4dc3deb988f0173718ab165b21f86f17b1fe90
asahtik <asahtik@gmail.com>
2024-09-20 14:05:41 +0200

    Minor build improvements

--- preconf.sh
+++ preconf.sh
@@ -3,4 +3,7 @@ aclocal
 autoheader
 automake --foreign --copy --add-missing
 autoconf
-CFLAGS="-O2 -Wall -W -Wunused-const-variable=0 -pipe -g" ./configure --disable-aload
+mkdir -p build
+cd build
+CFLAGS="-O2 -Wall -W -Wunused-const-variable=0 -pipe -g" ../configure
+cd ..
